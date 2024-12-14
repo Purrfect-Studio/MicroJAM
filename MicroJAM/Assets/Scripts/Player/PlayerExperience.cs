1d56f6eabfdd2a4612cd5325efc30aa098f8388b
Caetano Costa <caetanogonca@gmail.com>
2024-12-14 16:07:02 -0300

    barra de xp funcionando

--- MicroJAM/Assets/Scripts/Player/PlayerExperience.cs
+++ MicroJAM/Assets/Scripts/Player/PlayerExperience.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerExperience : MonoBehaviour
 {
     public Slider xpBar;
+    public TextMeshProUGUI levelText;
     public float currentXP = 0;
     public float maxXP = 10;
+    private int currentLevel = 0;
+
 
     void Start()
     {
         xpBar.maxValue = maxXP;
         xpBar.value = currentXP;
+        levelText.text = "Level: " + currentLevel.ToString();
     }
 
     void Update()
     {
-        updateXpBar();
+        levelUp();
     }
 
-    void updateXpBar()
+    void levelUp()
     {
         if(currentXP >= maxXP)
         {
@@ -28,6 +33,8 @@ public class PlayerExperience : MonoBehaviour
             maxXP *= 1.2f;
             xpBar.maxValue = maxXP;
             xpBar.value = currentXP;
+            currentLevel += 1;
+            levelText.text = "Level: " + currentLevel.ToString();
         }
     }
 
