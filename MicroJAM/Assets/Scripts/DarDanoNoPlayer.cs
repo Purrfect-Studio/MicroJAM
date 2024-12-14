e906834b3ee8724b8434120cf431e36b7c834839
João Guilherme <joao.novaes@aluno.ifsp.edu.br>
2024-12-13 22:21:23 -0300

    kb certo

--- MicroJAM/Assets/Scripts/DarDanoNoPlayer.cs
+++ MicroJAM/Assets/Scripts/DarDanoNoPlayer.cs
@@ -1,32 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class DarDanoNoPlayer : MonoBehaviour
 {
     public PlayerHealthSystem playerHealthSystem;
     public PlayerController playerController;
-    public int damage;
-    //public Animator animator;
-    void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Player") && !playerController.isInvulnerable) //&& !playerController.isInvulnerable
+    public int damage = 1;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !playerController.isInvulnerable)
         {
-            playerController.KBCount = playerController.KBTime;
-            if (collision.transform.position.x <= transform.position.x)
-            {
-                playerController.isKnockRight = true;
-            } else
-            {
-                playerController.isKnockRight = false;
-            }
+            playerController.KBCount = playerController.KBTime; // Inicia o knockback
+            playerController.isKnockRight = collision.transform.position.x < transform.position.x; // Define a direção do knockback
 
+            playerHealthSystem.vida -= damage; // Reduz a vida do player
 
-            playerHealthSystem.vida -= damage;
-
-            //animator.SetTrigger("attack");
-
-            //StartCoroutine(playerController.InvulnerabilityCoroutine());
+            StartCoroutine(playerController.InvulnerabilityCoroutine()); // Ativa a invulnerabilidade
         }
     }
 }
--- MicroJAM/Assets/Scripts/PlayerController.cs
+++ MicroJAM/Assets/Scripts/PlayerController.cs
@@ -1,103 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRigidBody2D;
     public SpriteRenderer spriteRenderer;
-    //public AudioSource footStepAudioSource;
-    //public AudioClip[] soundEffects;
     public Animator playerAnimator;
     private Vector2 playerDirection;
     private Vector2 lastPlayerDirection;
-    public float playerSpeed;
-    public float KBForce;
-    public float KBCount;
-    public float KBTime;
+    public float playerSpeed = 5f;
+    public float KBForce = 10f;
+    public float KBCount = 0f; // Será inicializado na colisão
+    public float KBTime = 0.2f; // Duração do knockback
     public bool isKnockRight;
-    public float invulnerabilityDuration = 2f;
+    public float invulnerabilityDuration = 0.5f;
     public bool isInvulnerable = false;
-    //public float stepVolume = 0.05f;
-    public bool canMove = true;
 
     void Start()
     {   
         playerAnimator = GetComponent<Animator>();
         playerRigidBody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
     }
 
-    // Update is called once per frame
     void Update()
     {   
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        
-        if ( moveX == 0 && moveY == 0 && (playerDirection.x != 0 || playerDirection.y != 0)) {
 
+        if (moveX == 0 && moveY == 0 && (playerDirection.x != 0 || playerDirection.y != 0)) 
+        {
             lastPlayerDirection = playerDirection;
         }
 
         playerDirection = new Vector2(moveX, moveY).normalized;
-      
     }
 
     void FixedUpdate()
     {
-        KnockLogic();
+        Move();
     }
 
-    void KnockLogic(){
-        if (KBCount < 0)
+    void Move()
+    {
+        // Calcula a velocidade de movimento normal
+        Vector2 moveVelocity = playerDirection * playerSpeed;
+
+        // Se ainda estiver em knockback, adiciona o knockback à velocidade
+        if (KBCount > 0) 
         {
-            Move();
-        } else
-        {
-            if (isKnockRight)
-            {
-                playerRigidBody2D.velocity = new Vector2(-KBCount, KBForce);
-            } else
-            {
-                playerRigidBody2D.velocity = new Vector2(+KBCount, KBForce);
-            }
+            float knockbackX = isKnockRight ? -1 : 1; // Define a direção do knockback (esquerda ou direita)
+            Vector2 knockbackVelocity = new Vector2(knockbackX * KBForce, 0);
+            moveVelocity += knockbackVelocity; // Soma a velocidade do knockback
+            KBCount -= Time.fixedDeltaTime; // Diminui o tempo de knockback
         }
-        KBCount -= Time.deltaTime;
+
+        // Move o jogador com a soma da velocidade do movimento e do knockback
+        playerRigidBody2D.velocity = moveVelocity;
     }
-
-
-    void Move(){
-
-        playerRigidBody2D.MovePosition(playerRigidBody2D.position + playerDirection * playerSpeed * Time.fixedDeltaTime);
-        
-        if(true){ //footStepAudioSource != null
-            if (playerDirection.sqrMagnitude > 0 && !footStepAudioSource.isPlaying)
-            {
-                //footStepAudioSource.clip = soundEffects[0];
-                //footStepAudioSource.loop = true;
-                //footStepAudioSource.Play();
-            } 
-
-            if(playerDirection.sqrMagnitude <= 00)//&& footStepAudioSource.isPlaying
-            {
-                //footStepAudioSource.Stop();
-            }
-        }
-    }
-
-
 
     public IEnumerator InvulnerabilityCoroutine()
     {
         isInvulnerable = true;
         Color originalColor = spriteRenderer.color;
-        spriteRenderer.color = Color.red; // Troca a cor para vermelho ou qualquer outra cor de sua escolha
+        spriteRenderer.color = Color.red; // Indica que o player está invulnerável
 
         yield return new WaitForSeconds(invulnerabilityDuration);
 
-        spriteRenderer.color = originalColor; // Retorna à cor original
+        spriteRenderer.color = originalColor; // Retorna a cor original
         isInvulnerable = false;
     }
 }
