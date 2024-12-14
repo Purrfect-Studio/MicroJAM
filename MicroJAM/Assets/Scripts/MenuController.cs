2ab3665b375a0e97d52ec9bb91359e01a55e5469
Erick Santos Brito <brito.santos@aluno.ifsp.edu.br>
2024-12-13 21:23:10 -0300

    [Menu]: Criando a base do menu principal

---
+++ MicroJAM/Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    public string cena;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void QuitGame()
+    {
+        // Editor Unity
+        // UnityEditor.EditorApplication.isPlaying = false;
+        
+        // Para o jogo compilado
+        Application.Quit();
+    }
+    
+    private bool isFullscreen = true;
+
+    public void ToggleScreenMode()
+    {
+        isFullscreen = !isFullscreen;
+        Screen.fullScreen = isFullscreen;
+    }
+}
