4c05b92a5fa8130c0dd07a2a23b70ba1f3956a65
Caetano Costa <caetanogonca@gmail.com>
2024-12-15 18:18:48 -0300

    aa

--- MicroJAM/Assets/Scripts/Enemy/EnemyAI.cs
+++ MicroJAM/Assets/Scripts/Enemy/EnemyAI.cs
@@ -9,7 +9,7 @@ public class EnemyAI : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        Physics2D.IgnoreLayerCollision(3, 7);
+        Physics2D.IgnoreLayerCollision(10, 7);
     }
 
     // Update is called once per frame
