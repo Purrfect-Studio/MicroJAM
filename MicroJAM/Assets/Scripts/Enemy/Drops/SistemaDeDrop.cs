88218fce0d2547a78e3d7249ea209003d3c339d6
Caetano Costa <caetanogonca@gmail.com>
2024-12-15 14:08:52 -0300

    ajustes no sistema de drops

--- MicroJAM/Assets/Scripts/Enemy/Drops/SistemaDeDrop.cs
+++ MicroJAM/Assets/Scripts/Enemy/Drops/SistemaDeDrop.cs
@@ -21,46 +21,41 @@ public class SistemaDeDrop : MonoBehaviour
     private int escolherDrop()
     {
         string nomeDaCena = SceneManager.GetActiveScene().name;
-        if(nomeDaCena == "Level 01"){
         probabilidade = Random.Range(0, 101);
-
-        if(probabilidade < 70)
-        {
-            return 0;
-        }else if(probabilidade >= 70 && probabilidade <= 85)
-        {
-            return 1;
-        }
-        else if (probabilidade >= 85 && probabilidade < 90 )
-        {
-            return 2;
-        }
-        else
-        {
+        if (nomeDaCena == "Level 01"){
+            if(probabilidade < 70)
+            {
+                return 0;
+            }else if(probabilidade >= 70 && probabilidade <= 85)
+            {
+                return 1;
+            }
+            else if (probabilidade >= 85 && probabilidade < 90 )
+            {
+                return 2;
+            }else 
+            {
             return 3;
-        }
-        }
-        else if(nomeDaCena == "Level 02"){
-             probabilidade = Random.Range(0, 101);
-
-        if(probabilidade < 20)
-        {
-            return 0;
-        }else if(probabilidade >= 20 && probabilidade <= 40)
-        {
-            return 1;
-        }
-        else if (probabilidade >= 40 && probabilidade < 60 )
-        {
-            return 2;
-        }
-        else
-        {
-            return 3;
-        }
-        } else{
-            return 2;
-        }
-
+            }
+        }else if(nomeDaCena == "Level 02"){
+            if(probabilidade < 20)
+            {
+                return 0;
+            }else if(probabilidade >= 20 && probabilidade < 75)
+            {
+                return 1;
+            }
+            else if (probabilidade >= 75 && probabilidade < 90 )
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+        }else
+            {
+                return 0;
+            }
     }
 }
