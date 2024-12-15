4b94ff786fe81ee029337dc54b75d328acd5d26d
Caetano Costa <caetanogonca@gmail.com>
2024-12-15 17:21:22 -0300

    aa

--- MicroJAM/Assets/Scripts/Player/PlayerHealthSystem.cs
+++ MicroJAM/Assets/Scripts/Player/PlayerHealthSystem.cs
@@ -12,9 +12,12 @@ public class PlayerHealthSystem : MonoBehaviour
 
     private Camera mainCamera; // Referência à câmera principal
     public CurrentPlayerStats currentPlayerStats;
+    public GameObject canvasGameOver;
+    private bool estaMorto;
 
     void Start()
     {
+        estaMorto = false;
         maxHealth = currentPlayerStats.maxHealth;
         currentHealth = maxHealth;
         // Procura o Slider na cena pela tag "HealthBar"
@@ -61,9 +64,13 @@ public class PlayerHealthSystem : MonoBehaviour
 
     void CheckDeath()
     {
-        if (currentHealth <= 0)
+        if (currentHealth <= 0 && !estaMorto)
         {
+            canvasGameOver.SetActive(true);
+            Time.timeScale = 0;
             Debug.Log("Player morreu");
+            estaMorto = true;
+            gameObject.SetActive(false);
         }
     }
 
